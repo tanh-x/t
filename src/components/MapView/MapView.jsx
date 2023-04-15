8dc41209c8bbfa9ccbdd8d35f26d8515f7086cd5
Thanh <kewber0@gmail.com>
2023-04-15 17:15:26 -0500

    dark-map

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -9,10 +9,11 @@ const MapView = (props) => {
                 scrolling="no"
                 marginheight="0"
                 marginwidth="0"
-                src="https://www.openstreetmap.org/export/embed.html?bbox=-93.661848084795%2C42.02137874987738%2C-93.63410331474495%2C42.032560237415204&amp;layer=mapnik" />
+                src="https://api.maptiler.com/maps/b3a3ff72-58c6-45c4-94b9-aea1b5b482e8/?key=nsgkxLq2pn7VwkNHWDEJ#14.5/42.02653/-93.64449" />
             <br />
         </div>
     );
 };
 
+// src="https://www.openstreetmap.org/export/embed.html?bbox=-93.661848084795%2C42.02137874987738%2C-93.63410331474495%2C42.032560237415204&amp;layer=mapnik" />
 export default MapView;
