3f4546ebf36e2c236a5b38cf35d733b1dccb5f97
Thanh <kewber0@gmail.com>
2023-04-16 02:56:56 -0500

    dsapjfdfj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import 'bootstrap-dark/dist/js/darkmode.min.js';
-// import "bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import "bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import { useState } from "react";
 import Overlay from "./components/Overlay";
 import MainContent from "./components/MainContent";
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+
+import "./Sidebar.css";
 
 
 const halls = {
@@ -52,54 +55,56 @@ const halls = {
 }
 
 const Sidebar = (props) => {
-  return <div className="sidebar">
-    <Navbar variant="dark" style={{
-      borderRadius: "8px",
-      marginBottom: "1em",
-      fontSize: "1.25em",
-      background: "#3B3B3B32",
-    }}>
-      <Container>
-        <Navbar.Brand style={{ fontSize: "1.5em", marginLeft: "0.3em", marginRight: "0.667em" }}>SitWithMe</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link >Home</Nav.Link>
-          <Nav.Link >About</Nav.Link>
-          <NavDropdown title="Things" id="navbarScrollingDropdown"> 
-            <NavDropdown.Item>Action</NavDropdown.Item>
-            <NavDropdown.Item>
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown>
+  return <>
+    <div className="sidebar">
+      <Navbar variant="dark" style={{
+        borderRadius: "8px",
+        marginBottom: "1em",
+        fontSize: "1.25em",
+        background: "#3B3B3B32",
+      }}>
+        <Container>
+          <Navbar.Brand style={{ fontSize: "1.5em", marginLeft: "0.3em", marginRight: "0.667em" }}>SitWithMe</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link >Home</Nav.Link>
+            <Nav.Link >About</Nav.Link>
+            <NavDropdown title="Things" id="navbarScrollingDropdown">
+              <NavDropdown.Item>Action</NavDropdown.Item>
+              <NavDropdown.Item>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
 
-        </Nav>
-      </Container>
-    </Navbar>
-    <Accordion variant="dark">
-      {Object.entries(halls).map(([k, v], i) =>
-        <Accordion.Item eventKey={i}>
-          <Accordion.Header onClick={() => {
-            props.toggleLocation(k)
-          }}>
-            <div className="class-header">
-              <h4>{k}</h4>
-              <strong style={{ fontSize: "1.66em" }}>
-                {v.numPeople}
-              </strong>
-            </div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <span style={{ fontSize: "1.33em" }}>
-              {v.description ?? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-            </span>
-          </Accordion.Body>
-        </Accordion.Item>
-      )}
-    </Accordion>
-  </div>;
+          </Nav>
+        </Container>
+      </Navbar>
+      <Accordion>
+        {Object.entries(halls).map(([k, v], i) =>
+          <Accordion.Item eventKey={i} variant="custom">
+            <Accordion.Header onClick={() => {
+              props.toggleLocation(k)
+            }}>
+              <div className="class-header">
+                <h4>{k}</h4>
+                <strong style={{ fontSize: "1.5em" }}>
+                  {v.numPeople}
+                </strong>
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <span style={{ fontSize: "1.33em" }}>
+                {v.description ?? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+              </span>
+            </Accordion.Body>
+          </Accordion.Item>
+        )}
+      </Accordion>
+    </div>
+  </>;
 }
 
 export default Sidebar;
