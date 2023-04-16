21bc6714e910336ddad1c684a76259f13eed6a1e
William Clemmons Jr <william.clemmons.jr@gmail.com>
2023-04-15 23:16:09 -0500

    Got firebase working
    Created DataAccessService.js to make doing server request easy
    
    Stuck on reading data from the server

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {useEffect, useState} from "react";
-import {initializeApp} from "firebase/app";
 import Overlay from "./components/Overlay";
 import Underlay from "./components/Underlay";
-import StorageTest from "./components/StorageTest";
+import {DataAccessService} from "./DataAccessService";
+import {useEffect} from "react";
 
 function App() {
-    const [selectedLocation, changeLocation] = useState(null);
-    const [firebaseApp, changeFirebaseApp] = useState(null);
-    const firebaseConfig = {
-        apiKey: "AIzaSyCfTzr1Bb6vQEDMAMLW6k1IxkS4NBO89Eg",
-        authDomain: "sit-with-me-ab673.firebaseapp.com",
-        projectId: "sit-with-me-ab673",
-        storageBucket: "sit-with-me-ab673.appspot.com",
-        messagingSenderId: "626380445969",
-        appId: "1:626380445969:web:a9fc3c95c8ff41d51114b0"
-    };
-    changeFirebaseApp(initializeApp(firebaseConfig))
-
     // TODO: Add SDKs for Firebase products that you want to use
-    useEffect(() => {
-
-    }, [firebaseApp]);
+    useEffect( () => {
+        let out = DataAccessService.GetTables().then(out => {
+            out.forEach()
+        })
+        // out.forEach((doc) => {
+        //     console.log(doc.id, " => ", doc.data());
+        // })
+        return () => {}
+    })
     return (
         <div className="App">
-            <StorageTest app={firebaseApp}/>
             <Overlay/>
             <Underlay/>
         </div>
